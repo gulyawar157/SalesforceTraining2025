6a4eb04d8ca8b801546106426985aa220e0eeb0f
gulyawar.aman <gulyawar.aman@ITT-GULYAWAR.intimetec.americas>
2025-09-17 15:33:32 +0530

    New Changes

---
+++ ServletfirstDemo/src/main/java/in/sp/backend/MyServlet.java
@@ -0,0 +1,21 @@
+package in.sp.backend;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+     @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	
+    	 PrintWriter out = response.getWriter();
+    	 out.print("I am inside doGet() method inside browser");
+    	 
+    	 System.out.println("I am inside doGet() method");
+    	 
+    }
+}
